3ec3d4d2eacdd56e8cac6970626cd674237d75d1
huangliming <huangliming5@huawei.com>
2021-11-05 16:42:58 +0800

    add client-go
    Signed-off-by: huangliming <huangliming5@huawei.com>

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.16
 
 require (
 	github.com/cilium/ebpf v0.7.0
+	github.com/jonboulle/clockwork v0.2.2 // indirect
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.5 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.8.1
+	k8s.io/api v0.22.3
+	k8s.io/apimachinery v0.22.3
+	k8s.io/client-go v0.22.3
 )
--- pkg/policy/policy.go
+++ pkg/policy/policy.go
@@ -1,12 +1,26 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
- * Description: 
+ * Description:
  */
 
 package policy
 
 import (
 	"codehub.com/mesh/pkg/logger"
+	"fmt"
+	apiCoreV1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/informers"
+	informersCoreV1 "k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/kubernetes"
+	listerCoreV1 "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/util/workqueue"
+	"path/filepath"
+	"time"
 )
 
 const (
@@ -17,8 +31,156 @@ var (
 	log = logger.DefaultLogger.WithField(logger.LogSubsys, pkgSubsys)
 )
 
-func ControlManager() {
-	for true {
-		//TODO
+type KubeContorller struct {
+	factory informers.SharedInformerFactory
+	serviceInformer informersCoreV1.ServiceInformer
+	endpointInformer informersCoreV1.EndpointsInformer
+	serviceLister listerCoreV1.ServiceLister
+	endpointLister listerCoreV1.EndpointsLister
+	queue workqueue.RateLimitingInterface
+}
+
+func NewKubeContorller(clientset kubernetes.Interface) *KubeContorller {
+	factory := informers.NewSharedInformerFactory(clientset, time.Second * 30)
+
+	contorller := &KubeContorller{
+		factory: factory,
+		serviceInformer: factory.Core().V1().Services(),
+		endpointInformer: factory.Core().V1().Endpoints(),
+		queue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Services"),
 	}
+
+	//contorller.serviceInformer.Lister()
+	contorller.serviceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: contorller.enqueue,
+		UpdateFunc: contorller.enqueueForUpdate,
+		DeleteFunc: contorller.enqueueForDelete,
+	})
+
+	return contorller
+}
+
+func (c *KubeContorller) syncHandler(key string) error {
+	obj, exists, err := c.serviceInformer.Informer().GetIndexer().GetByKey(key)
+	if err != nil {
+		return fmt.Errorf("get object with key %s from store failed with %v", key, err)
+	}
+
+	if !exists {
+		fmt.Printf("Service %s does not exist anymore\n", key)
+	} else {
+		fmt.Printf("Sync/Add/Update for service: %s\n", obj.(*apiCoreV1.Service).GetName())
+		fmt.Printf("Sync/Add/Update for service: %s\n", obj.(*apiCoreV1.Service))
+	}
+
+	return nil
+}
+
+func (c *KubeContorller) processNextWorkItem() bool {
+	obj, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+
+	err := func(obj interface{}) error {
+		defer c.queue.Done(obj)
+
+		key, ok := obj.(string)
+		if !ok {
+			c.queue.Forget(obj)
+			return fmt.Errorf("expected string in queue but got %#v", obj)
+		}
+
+		if err := c.syncHandler(key); err != nil {
+			return fmt.Errorf("sync failed %s: %s", key, err.Error())
+		}
+
+		c.queue.Forget(obj)
+		return nil
+	}(obj)
+
+	if err != nil {
+		log.Debug(err)
+		return false
+	}
+
+	return true
+}
+
+func (c *KubeContorller) runWorker() {
+	for c.processNextWorkItem() {
+	}
+}
+
+func (c *KubeContorller) Run(stopCh <-chan struct{}) error {
+	defer c.queue.ShutDown()
+
+	go c.factory.Start(stopCh)
+
+	if ok := cache.WaitForCacheSync(stopCh, c.serviceInformer.Informer().HasSynced); !ok {
+		return fmt.Errorf("kube wait for caches to sync failed")
+	}
+
+	go wait.Until(c.runWorker, time.Second, stopCh)
+
+	<-stopCh
+	return nil
+}
+
+func (c *KubeContorller) enqueue(obj interface{}) {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.queue.AddRateLimited(key)
+}
+
+func (c *KubeContorller) enqueueForUpdate(oldObj, newObj interface{}) {
+	oldSer := oldObj.(*apiCoreV1.Service)
+	newSer := newObj.(*apiCoreV1.Service)
+	if oldSer == newSer {
+		return
+	}
+	c.enqueue(newObj)
+}
+
+func (c *KubeContorller) enqueueForDelete(obj interface{}) {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != err {
+		runtime.HandleError(err)
+		return
+	}
+	c.queue.AddRateLimited(key)
+}
+
+func kubeClient() error {
+	home := homedir.HomeDir()
+	if home == "" {
+		return fmt.Errorf("kube get home failed")
+	}
+	kubeconfig := filepath.Join(home, ".kube", "config")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return fmt.Errorf("kube build config failed")
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("kube now clientset failld")
+	}
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	contorller := NewKubeContorller(clientset)
+	if err := contorller.Run(stopCh); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ControlManager() {
+	kubeClient()
 }
