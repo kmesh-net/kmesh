b226f42894a34f170df11959cce7572a10f9660e
huangliming <huangliming5@huawei.com>
2021-11-10 09:27:22 +0800

    KubeController add endpointInformer
    Signed-off-by: huangliming <huangliming5@huawei.com>

--- pkg/policy/policy.go
+++ pkg/policy/policy.go
@@ -14,7 +14,6 @@ import (
 	"k8s.io/client-go/informers"
 	informersCoreV1 "k8s.io/client-go/informers/core/v1"
 	"k8s.io/client-go/kubernetes"
-	listerCoreV1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -25,58 +24,159 @@ import (
 
 const (
 	pkgSubsys = "policy"
+	InformerNameService = "Service"
+	InformerNameEndpoints = "Endpoints"
 )
 
 var (
 	log = logger.DefaultLogger.WithField(logger.LogSubsys, pkgSubsys)
 )
 
-type KubeContorller struct {
-	factory informers.SharedInformerFactory
-	serviceInformer informersCoreV1.ServiceInformer
-	endpointInformer informersCoreV1.EndpointsInformer
-	serviceLister listerCoreV1.ServiceLister
-	endpointLister listerCoreV1.EndpointsLister
-	queue workqueue.RateLimitingInterface
+type KubeController struct {
+	queue		workqueue.RateLimitingInterface
+	factory		informers.SharedInformerFactory
+	serviceInformer		informersCoreV1.ServiceInformer
+	endpointInformer	informersCoreV1.EndpointsInformer
 }
 
-func NewKubeContorller(clientset kubernetes.Interface) *KubeContorller {
+type queueKey struct {
+	typ		string
+	opt		string
+	name	string
+}
+
+func NewKubeController(clientset kubernetes.Interface) *KubeController {
 	factory := informers.NewSharedInformerFactory(clientset, time.Second * 30)
 
-	contorller := &KubeContorller{
+	c := &KubeController{
 		factory: factory,
 		serviceInformer: factory.Core().V1().Services(),
 		endpointInformer: factory.Core().V1().Endpoints(),
-		queue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Services"),
+		queue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "KubeController"),
 	}
 
-	//contorller.serviceInformer.Lister()
-	contorller.serviceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: contorller.enqueue,
-		UpdateFunc: contorller.enqueueForUpdate,
-		DeleteFunc: contorller.enqueueForDelete,
-	})
+	handler := cache.ResourceEventHandlerFuncs{
+		AddFunc: c.enqueueForAdd,
+		UpdateFunc: c.enqueueForUpdate,
+		DeleteFunc: c.enqueueForDelete,
+	}
+	c.serviceInformer.Informer().AddEventHandler(handler)
+	c.endpointInformer.Informer().AddEventHandler(handler)
 
-	return contorller
+	return c
 }
 
-func (c *KubeContorller) syncHandler(key string) error {
-	obj, exists, err := c.serviceInformer.Informer().GetIndexer().GetByKey(key)
-	if err != nil {
-		return fmt.Errorf("get object with key %s from store failed with %v", key, err)
+func (c *KubeController) getObjectType(obj interface{}) string {
+	switch obj.(type) {
+	case *apiCoreV1.Service:
+		return InformerNameService
+	case *apiCoreV1.Endpoints:
+		return InformerNameEndpoints
+	default:
+		return ""
+	}
+}
+
+func (c *KubeController) checkObjectValidity(oldObj, newObj interface{}) bool {
+	if oldObj == newObj {
+		return false
 	}
 
+	switch newObj.(type) {
+	case *apiCoreV1.Service:
+		return true
+	case *apiCoreV1.Endpoints:
+		for _, subset := range newObj.(*apiCoreV1.Endpoints).Subsets {
+			for _, addr := range subset.Addresses {
+				if addr.IP != "" {
+					return true
+				}
+			}
+		}
+	default:
+	}
+
+	return false
+}
+
+func (c *KubeController) enqueue(opt, typ, name string) {
+	key := queueKey{
+		opt: opt,
+		typ: typ,
+		name: name,
+	}
+	c.queue.AddRateLimited(key)
+}
+
+func (c *KubeController) enqueueForAdd(obj interface{}) {
+	name, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.enqueue("Add", c.getObjectType(obj), name)
+}
+
+func (c *KubeController) enqueueForUpdate(oldObj, newObj interface{}) {
+	if !c.checkObjectValidity(oldObj, newObj) {
+		return
+	}
+
+	name, err := cache.MetaNamespaceKeyFunc(newObj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.enqueue("Update", c.getObjectType(newObj), name)
+}
+
+func (c *KubeController) enqueueForDelete(obj interface{}) {
+	name, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != err {
+		runtime.HandleError(err)
+		return
+	}
+	c.enqueue("Delete", c.getObjectType(obj), name)
+}
+
+func (c *KubeController) syncHandler(key queueKey) error {
+	var (
+		err error
+		exists bool
+		obj interface{}
+		serviceObj *apiCoreV1.Service
+		endpointsObj *apiCoreV1.Endpoints
+	)
+
+	switch key.typ {
+	case InformerNameService:
+		obj, exists, err = c.serviceInformer.Informer().GetIndexer().GetByKey(key.name)
+		if err == nil && exists {
+			serviceObj = obj.(*apiCoreV1.Service)
+			fmt.Printf("syncHandler for Service: %#v\n", serviceObj)
+		}
+	case InformerNameEndpoints:
+		obj, exists, err = c.endpointInformer.Informer().GetIndexer().GetByKey(key.name)
+		if err == nil && exists {
+			endpointsObj = obj.(*apiCoreV1.Endpoints)
+			fmt.Printf("syncHandler for Endpoints: %#v\n", endpointsObj)
+		}
+	default:
+		return fmt.Errorf("invlid queueKey name")
+	}
+
+	if err != nil {
+		return fmt.Errorf("get object with key %#v from store failed with %v", key, err)
+	}
 	if !exists {
-		fmt.Printf("Service %s does not exist anymore\n", key)
-	} else {
-		fmt.Printf("Sync/Add/Update for service: %s\n", obj.(*apiCoreV1.Service).GetName())
-		fmt.Printf("Sync/Add/Update for service: %s\n", obj.(*apiCoreV1.Service))
+		fmt.Printf("Service or Endpoints %#v does not exist anymore\n", key)
+		return nil
 	}
 
 	return nil
 }
 
-func (c *KubeContorller) processNextWorkItem() bool {
+func (c *KubeController) processNextWorkItem() bool {
 	obj, shutdown := c.queue.Get()
 	if shutdown {
 		return false
@@ -85,7 +185,7 @@ func (c *KubeContorller) processNextWorkItem() bool {
 	err := func(obj interface{}) error {
 		defer c.queue.Done(obj)
 
-		key, ok := obj.(string)
+		key, ok := obj.(queueKey)
 		if !ok {
 			c.queue.Forget(obj)
 			return fmt.Errorf("expected string in queue but got %#v", obj)
@@ -107,12 +207,12 @@ func (c *KubeContorller) processNextWorkItem() bool {
 	return true
 }
 
-func (c *KubeContorller) runWorker() {
+func (c *KubeController) runWorker() {
 	for c.processNextWorkItem() {
 	}
 }
 
-func (c *KubeContorller) Run(stopCh <-chan struct{}) error {
+func (c *KubeController) Run(stopCh <-chan struct{}) error {
 	defer c.queue.ShutDown()
 
 	go c.factory.Start(stopCh)
@@ -127,34 +227,7 @@ func (c *KubeContorller) Run(stopCh <-chan struct{}) error {
 	return nil
 }
 
-func (c *KubeContorller) enqueue(obj interface{}) {
-	key, err := cache.MetaNamespaceKeyFunc(obj)
-	if err != nil {
-		runtime.HandleError(err)
-		return
-	}
-	c.queue.AddRateLimited(key)
-}
-
-func (c *KubeContorller) enqueueForUpdate(oldObj, newObj interface{}) {
-	oldSer := oldObj.(*apiCoreV1.Service)
-	newSer := newObj.(*apiCoreV1.Service)
-	if oldSer == newSer {
-		return
-	}
-	c.enqueue(newObj)
-}
-
-func (c *KubeContorller) enqueueForDelete(obj interface{}) {
-	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-	if err != err {
-		runtime.HandleError(err)
-		return
-	}
-	c.queue.AddRateLimited(key)
-}
-
-func kubeClient() error {
+func KubeClient() error {
 	home := homedir.HomeDir()
 	if home == "" {
 		return fmt.Errorf("kube get home failed")
@@ -173,8 +246,8 @@ func kubeClient() error {
 	stopCh := make(chan struct{})
 	defer close(stopCh)
 
-	contorller := NewKubeContorller(clientset)
-	if err := contorller.Run(stopCh); err != nil {
+	controller := NewKubeController(clientset)
+	if err := controller.Run(stopCh); err != nil {
 		return err
 	}
 
@@ -182,5 +255,5 @@ func kubeClient() error {
 }
 
 func ControlManager() {
-	kubeClient()
+	KubeClient()
 }
