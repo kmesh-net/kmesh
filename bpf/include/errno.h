e83024cf72a027078f98c30d4ba402312b05d2b8
huangliming <huangliming5@huawei.com>
2021-11-12 16:55:09 +0800

    add license
    Signed-off-by: huangliming <huangliming5@huawei.com>

--- bpf/include/errno.h
+++ bpf/include/errno.h
@@ -1,6 +1,15 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
- * Description: 
+ * Copyright (c) 2019 Huawei Technologies Co., Ltd.
+ * MeshAccelerating is licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author: LemmyHuang
+ * Create: 2021-09-17
  */
 
 #ifndef _ERRNO_H_
