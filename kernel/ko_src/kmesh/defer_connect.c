92695c8f42952f0bdb6e422de7ef7bf55587f215
Di Zhu <zhudi2@huawei.com>
2022-10-21 11:32:04 +0800

    fix sca problem
    Signed-off-by: Di Zhu <zhudi2@huawei.com>

--- kernel/ko_src/kmesh/defer_connect.c
+++ kernel/ko_src/kmesh/defer_connect.c
@@ -153,146 +153,29 @@ static int defer_inet_sendmsg(struct socket *sock, struct msghdr *msg, size_t si
 			       sk, msg, size);
 }
 
-static long inet_wait_for_connect(struct sock *sk, long timeo, int writebias)
-{
-	DEFINE_WAIT_FUNC(wait, woken_wake_function);
-
-	add_wait_queue(sk_sleep(sk), &wait);
-	sk->sk_write_pending += writebias;
-
-	/* Basic assumption: if someone sets sk->sk_err, he _must_
-	 * change state of the socket from TCP_SYN_*.
-	 * Connect() does not allow to get error notifications
-	 * without closing the socket.
-	 */
-	while ((1 << sk->sk_state) & (TCPF_SYN_SENT | TCPF_SYN_RECV)) {
-		release_sock(sk);
-		timeo = wait_woken(&wait, TASK_INTERRUPTIBLE, timeo);
-		lock_sock(sk);
-		if (signal_pending(current) || !timeo)
-			break;
-	}
-	remove_wait_queue(sk_sleep(sk), &wait);
-	sk->sk_write_pending -= writebias;
-	return timeo;
-}
-
 static int __defer_inet_stream_connect(struct socket *sock, struct sockaddr *uaddr,
 			  int addr_len, int flags, int is_sendmsg)
 {
 	struct sock *sk = sock->sk;
 	int err;
-	long timeo;
 
-	/*
-	 * uaddr can be NULL and addr_len can be 0 if:
-	 * sk is a TCP fastopen active socket and
-	 * TCP_FASTOPEN_CONNECT sockopt is set and
-	 * we already have a valid cookie for this socket.
-	 * In this case, user can call write() after connect().
-	 * write() will invoke tcp_sendmsg_fastopen() which calls
-	 * __inet_stream_connect().
-	 */
-	if (uaddr) {
-		if (addr_len < sizeof(uaddr->sa_family))
-			return -EINVAL;
+	if (sock->state != SS_UNCONNECTED)
+		goto origin_conn;
 
-		if (uaddr->sa_family == AF_UNSPEC) {
-			err = sk->sk_prot->disconnect(sk, flags);
-			sock->state = err ? SS_DISCONNECTING : SS_UNCONNECTED;
-			goto out;
+	if (BPF_CGROUP_PRE_CONNECT_ENABLED(sk)) {
+		err = sk->sk_prot->pre_connect(sk, uaddr, addr_len);
+		if (err == DEFER_CONNECT) {
+			inet_sk(sk)->bpf_defer_connect = 1;
+			sk->sk_dport = ((struct sockaddr_in *)uaddr)->sin_port;
+			sk_daddr_set(sk, ((struct sockaddr_in *)uaddr)->sin_addr.s_addr);
+			sock->state = SS_CONNECTING;
+			tcp_set_state(sk, TCP_SYN_SENT);
+			return 0;
 		}
 	}
 
-	switch (sock->state) {
-	default:
-		err = -EINVAL;
-		goto out;
-	case SS_CONNECTED:
-		err = -EISCONN;
-		goto out;
-	case SS_CONNECTING:
-		if (inet_sk(sk)->defer_connect || inet_sk(sk)->bpf_defer_connect)
-			err = is_sendmsg ? -EINPROGRESS : -EISCONN;
-		else
-			err = -EALREADY;
-		/* Fall out of switch with err, set for this state */
-		break;
-	case SS_UNCONNECTED:
-		err = -EISCONN;
-		if (sk->sk_state != TCP_CLOSE)
-			goto out;
-
-		if (BPF_CGROUP_PRE_CONNECT_ENABLED(sk)) {
-			err = sk->sk_prot->pre_connect(sk, uaddr, addr_len);
-			if (err == DEFER_CONNECT) {
-				inet_sk(sk)->bpf_defer_connect = 1;
-				err = 0;
-				sk->sk_dport = ((struct sockaddr_in *)uaddr)->sin_port;
-				sk_daddr_set(sk, ((struct sockaddr_in *)uaddr)->sin_addr.s_addr);
-				sock->state = SS_CONNECTING;
-				tcp_set_state(sk, TCP_SYN_SENT);
-				goto out;
-			}
-			if (err)
-				goto out;
-		}
-
-		err = sk->sk_prot->connect(sk, uaddr, addr_len);
-		if (err < 0)
-			goto out;
-
-		sock->state = SS_CONNECTING;
-		if (!err && inet_sk(sk)->defer_connect) {
-			goto out;
-		}
-
-		/* Just entered SS_CONNECTING state; the only
-		 * difference is that return value in non-blocking
-		 * case is EINPROGRESS, rather than EALREADY.
-		 */
-		err = -EINPROGRESS;
-		break;
-	}
-
-	timeo = sock_sndtimeo(sk, flags & O_NONBLOCK);
-
-	if ((1 << sk->sk_state) & (TCPF_SYN_SENT | TCPF_SYN_RECV)) {
-		int writebias = (sk->sk_protocol == IPPROTO_TCP) &&
-				tcp_sk(sk)->fastopen_req &&
-				tcp_sk(sk)->fastopen_req->data ? 1 : 0;
-
-		/* Error code is set above */
-		if (!timeo || !inet_wait_for_connect(sk, timeo, writebias))
-			goto out;
-
-		err = sock_intr_errno(timeo);
-		if (signal_pending(current))
-			goto out;
-	}
-
-	/* Connection was closed by RST, timeout, ICMP error
-	 * or another process disconnected us.
-	 */
-	if (sk->sk_state == TCP_CLOSE)
-		goto sock_error;
-
-	/* sk->sk_err may be not zero now, if RECVERR was ordered by user
-	 * and error was received after socket entered established state.
-	 * Hence, it is handled normally after connect() return successfully.
-	 */
-
-	sock->state = SS_CONNECTED;
-	err = 0;
-out:
-	return err;
-
-sock_error:
-	err = sock_error(sk) ? : -ECONNABORTED;
-	sock->state = SS_UNCONNECTED;
-	if (sk->sk_prot->disconnect(sk, flags))
-		sock->state = SS_DISCONNECTING;
-	goto out;
+origin_conn:
+	return __inet_stream_connect(sock, uaddr, addr_len, flags, 0);
 }
 
 static int defer_inet_stream_connect(struct socket *sock, struct sockaddr *uaddr,
