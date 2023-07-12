3e142fba1f5e03f52cb0f3455d087a6a386baea3
superCharge <xiesongyang@huawei.com>
2023-07-12 16:48:09 +0800

    kmesh support tcp-weighted-cluster balance
    Signed-off-by: superCharge <xiesongyang@huawei.com>

--- bpf/kmesh/include/tcp_proxy.h
+++ bpf/kmesh/include/tcp_proxy.h
@@ -1,13 +1,17 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
- * MeshAccelerating is licensed under the Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
- *	 http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
- * See the Mulan PSL v2 for more details.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+
  * Author: supercharge
  * Create: 2023-06-15
  */
@@ -19,6 +23,53 @@
 #include "kmesh_common.h"
 #include "tail_call.h"
 
+static inline char *select_tcp_weight_cluster(const Filter__TcpProxy *tcpProxy)
+{
+	void *clusters = NULL;
+	Filter__TcpProxy__WeightedCluster *weightedClusters = NULL;
+	Filter__TcpProxy__WeightedCluster__ClusterWeight *cluster_weight = NULL;
+	int32_t select_value;
+	void *cluster_name = NULL;
+
+	weightedClusters = (Filter__TcpProxy__WeightedCluster *)kmesh_get_ptr_val((tcpProxy->weighted_clusters));
+	if (!weightedClusters) {
+		return NULL;
+	}
+	clusters = kmesh_get_ptr_val(weightedClusters->clusters);
+	if (!clusters) {
+		return NULL;
+	}
+
+	select_value = (int)(bpf_get_prandom_u32() % 100);
+	for (int i = 0; i < KMESH_PER_WEIGHT_CLUSTER_NUM; i ++) {
+		if (i >= weightedClusters->n_clusters) {
+			break;
+		}
+		cluster_weight = (Filter__TcpProxy__WeightedCluster__ClusterWeight *) kmesh_get_ptr_val(
+						(void *) *((__u64 *) clusters + i));
+		if (!cluster_weight) {
+			return NULL;
+		}
+		select_value = select_value - (int)cluster_weight->weight;
+		if (select_value <= 0) {
+			cluster_name = kmesh_get_ptr_val(cluster_weight->name);
+			BPF_LOG(DEBUG, FILTER, "select cluster, %s:%d\n", cluster_name, cluster_weight->weight);
+			return (char*)cluster_name;
+		}
+	}
+	return NULL;
+}
+
+
+static inline char *tcp_proxy_get_cluster(const Filter__TcpProxy *tcpProxy)
+{
+	if (tcpProxy->cluster_specifier_case == FILTER__TCP_PROXY__CLUSTER_SPECIFIER_WEIGHTED_CLUSTERS) {
+		return select_tcp_weight_cluster(tcpProxy);
+	}
+
+	return (char*)kmesh_get_ptr_val(tcpProxy->cluster);
+}
+
 static inline int tcp_proxy_manager(const Filter__TcpProxy *tcpProxy, ctx_buff_t *ctx)
 {
 	int ret;
@@ -26,9 +77,10 @@ static inline int tcp_proxy_manager(const Filter__TcpProxy *tcpProxy, ctx_buff_t
 	DECLARE_VAR_ADDRESS(ctx, addr);
 	ctx_key_t ctx_key = {0};
 	ctx_val_t ctx_val = {0};
+
 	if (NULL == tcpProxy)
 		return convert_sock_errno(-EINVAL);
-	cluster = (char *)kmesh_get_ptr_val(_(tcpProxy->cluster));
+	cluster = tcp_proxy_get_cluster(tcpProxy);
 	ctx_key.address = addr;
 	ctx_key.tail_call_index = KMESH_TAIL_CALL_CLUSTER + bpf_get_current_task();
 	ret = bpf_strcpy(ctx_val.data, BPF_DATA_MAX_LEN, cluster);
@@ -36,6 +88,7 @@ static inline int tcp_proxy_manager(const Filter__TcpProxy *tcpProxy, ctx_buff_t
 		BPF_LOG(ERR, FILTER, "failed to copy cluster %s\n", cluster);
 		return convert_sock_errno(ret);
 	}
+
 	ret = kmesh_tail_update_ctx(&ctx_key, &ctx_val);
 	if (ret != 0)
 		return convert_sock_errno(ret);
@@ -44,4 +97,5 @@ static inline int tcp_proxy_manager(const Filter__TcpProxy *tcpProxy, ctx_buff_t
 	kmesh_tail_delete_ctx(&ctx_key);
 	return 0;
 }
+
 #endif // __TCP_PROXY_H__
