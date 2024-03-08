27d8eabc8ca5e8395dad7ce0eb6ebe6469ac4276
kwb0523 <kwb0523@163.com>
2024-03-08 17:39:53 +0800

    add workload cache to be retrieved
    Signed-off-by: kwb0523 <kwb0523@163.com>

---
+++ pkg/controller/workload/workload_cache.go
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2024 The Kmesh Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package workload
+
+import (
+	"sync"
+
+	"google.golang.org/protobuf/proto"
+
+	workloadapi "kmesh.net/kmesh/api/v2/workloadapi"
+	nets "kmesh.net/kmesh/pkg/nets"
+)
+
+var (
+	WorkloadCache = newWorkloadStore()
+)
+
+type NetworkAddress struct {
+	Network string
+	Address uint32
+}
+
+type workloadStore struct {
+	byUid  map[string]*workloadapi.Workload
+	byAddr map[NetworkAddress]*workloadapi.Workload
+	mutex  sync.RWMutex
+}
+
+func newWorkloadStore() *workloadStore {
+	return &workloadStore{
+		byUid:  make(map[string]*workloadapi.Workload),
+		byAddr: make(map[NetworkAddress]*workloadapi.Workload),
+	}
+}
+
+func (w *workloadStore) GetWorkloadByUid(uid string) *workloadapi.Workload {
+	w.mutex.RLock()
+	defer w.mutex.RUnlock()
+	return w.byUid[uid]
+}
+
+func (w *workloadStore) GetWorkloadByAddr(networkAddress NetworkAddress) *workloadapi.Workload {
+	w.mutex.RLock()
+	defer w.mutex.RUnlock()
+	return w.byAddr[networkAddress]
+}
+
+func composeNetworkAddress(network string, addr uint32) NetworkAddress {
+	networkAddress := NetworkAddress{
+		Network: network,
+		Address: addr,
+	}
+
+	return networkAddress
+}
+
+func (w *workloadStore) addWorkload(workload *workloadapi.Workload) {
+	uid := workload.Uid
+
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	workloadByUid, exist := w.byUid[uid]
+	if exist {
+		if proto.Equal(workload, workloadByUid) {
+			return
+		}
+		// remove same uid but old address workload, avoid leak worklaod by address.
+		for _, ip := range workloadByUid.Addresses {
+			addr := nets.ConvertIpByteToUint32(ip)
+			networkAddress := composeNetworkAddress(workloadByUid.Network, addr)
+			delete(w.byAddr, networkAddress)
+		}
+	}
+
+	w.byUid[uid] = workload
+	for _, ip := range workload.Addresses {
+		addr := nets.ConvertIpByteToUint32(ip)
+		networkAddress := composeNetworkAddress(workload.Network, addr)
+		w.byAddr[networkAddress] = workload
+	}
+}
+
+func (w *workloadStore) deleteWorkload(uid string) {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	workload, exist := w.byUid[uid]
+	if exist {
+		for _, ip := range workload.Addresses {
+			addr := nets.ConvertIpByteToUint32(ip)
+			networkAddress := composeNetworkAddress(workload.Network, addr)
+			delete(w.byAddr, networkAddress)
+		}
+
+		delete(w.byUid, uid)
+	}
+}
