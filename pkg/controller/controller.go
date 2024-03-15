8d9aa7c75f86f4aaf1c90495111a84c947b8dd44
weli-l <1289113577@qq.com>
2024-03-15 14:19:08 +0800

    add bypass code
    Signed-off-by: weli-l <1289113577@qq.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -20,8 +20,10 @@ import (
 	"fmt"
 
 	"kmesh.net/kmesh/pkg/bpf"
+	"kmesh.net/kmesh/pkg/controller/bypass"
 	"kmesh.net/kmesh/pkg/controller/interfaces"
 	"kmesh.net/kmesh/pkg/logger"
+	"kmesh.net/kmesh/pkg/utils"
 )
 
 var (
@@ -37,6 +39,16 @@ func Start() error {
 	}
 
 	client = NewXdsClient()
+
+	clientset, err := utils.GetK8sclient()
+	if err != nil {
+		panic(err)
+	}
+
+	err = bypass.StartByPassController(clientset)
+	if err != nil {
+		return fmt.Errorf("failed to start bypass controller: %v", err)
+	}
 
 	return client.Run(stopCh)
 }
