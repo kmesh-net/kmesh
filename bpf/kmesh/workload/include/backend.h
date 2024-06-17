ae70e6561c9cdb3377d2f078f214bac635d13b8c
kangmingfa <1640528278@qq.com>
2024-06-17 16:31:41 +0800

    Fix bugs.
    Signed-off-by: kangmingfa <1640528278@qq.com>

--- bpf/kmesh/workload/include/backend.h
+++ bpf/kmesh/workload/include/backend.h
@@ -90,7 +90,7 @@ backend_manager(struct kmesh_context *kmesh_ctx, backend_value *backend_v, __u32
                         DEBUG,
                         BACKEND,
                         "get the backend addr=[%s:%u]\n",
-                        ip2str(&backend_v->addr.ip4, 1),
+                        ip2str(&kmesh_ctx->dnat_ip.ip4, 1),
                         bpf_ntohs(service_v->target_port[j]));
                     return 0;
                 }
