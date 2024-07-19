10bd30da56755a7b6d4581a5793b2c4fdb1e8135
Zhonghu Xu <xuzhonghu@huawei.com>
2024-07-19 10:44:01 +0800

    More update
    Signed-off-by: Zhonghu Xu <xuzhonghu@huawei.com>

--- pkg/controller/bypass/bypass_test.go
+++ pkg/controller/bypass/bypass_test.go
@@ -25,80 +25,12 @@ import (
 	"github.com/agiledragon/gomonkey/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+	"istio.io/api/annotation"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
-
-func TestPodKmeshLabelChangeTriggersByPassKmeshAction(t *testing.T) {
-	client := fake.NewSimpleClientset()
-
-	err := os.Setenv("NODE_NAME", "test_node")
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		os.Unsetenv("NODE_NAME")
-	})
-	stopCh := make(chan struct{})
-	c := NewByPassController(client)
-	go c.Run(stopCh)
-
-	var mu sync.Mutex
-	enabled := false
-	disabled := false
-
-	var wg sync.WaitGroup
-
-	patches := gomonkey.NewPatches()
-	defer patches.Reset()
-
-	patches.ApplyFunc(handleKmeshBypass, func(ns string, op int) error {
-		mu.Lock()
-		defer mu.Unlock()
-		if op == 1 {
-			enabled = true
-		} else {
-			disabled = true
-		}
-		// Signal that handleKmeshBypass has been called
-		wg.Done()
-		return nil
-	})
-
-	pod := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-pod",
-			Namespace: "default",
-			Labels: map[string]string{
-				"kmesh.net/bypass": "enabled",
-			},
-			Annotations: map[string]string{
-				"kmesh.net/redirection": "enabled",
-			},
-		},
-		Spec: corev1.PodSpec{
-			NodeName: "test-node",
-		},
-	}
-
-	wg.Add(1)
-	_, err = client.CoreV1().Pods("default").Create(context.TODO(), pod, metav1.CreateOptions{})
-	assert.NoError(t, err)
-
-	wg.Wait()
-	assert.Equal(t, true, enabled, "unexpected value for enabled flag")
-	assert.Equal(t, false, disabled, "unexpected value for disabled flag")
-
-	enabled = false
-	disabled = false
-
-	delete(pod.Labels, "kmesh.net/bypass")
-	wg.Add(1)
-	_, err = client.CoreV1().Pods("default").Update(context.TODO(), pod, metav1.UpdateOptions{})
-	assert.NoError(t, err)
-
-	wg.Wait()
-	assert.Equal(t, true, disabled, "unexpected value for enabled flag")
-}
 
 func TestPodSidecarLabelChangeTriggersAddIptablesAction(t *testing.T) {
 	client := fake.NewSimpleClientset()
@@ -111,9 +43,8 @@ func TestPodSidecarLabelChangeTriggersAddIptablesAction(t *testing.T) {
 	stopCh := make(<-chan struct{})
 	c := NewByPassController(client)
 	go c.Run(stopCh)
-	var mu sync.Mutex
-	enabled := false
-	disabled := false
+	enabled := atomic.NewBool(false)
+	disabled := atomic.NewBool(false)
 
 	var wg sync.WaitGroup
 
@@ -121,9 +52,7 @@ func TestPodSidecarLabelChangeTriggersAddIptablesAction(t *testing.T) {
 	defer patches1.Reset()
 
 	patches1.ApplyFunc(addIptables, func(ns string) error {
-		mu.Lock()
-		defer mu.Unlock()
-		enabled = true
+		enabled.Store(true)
 		// Signal that addIptables has been called
 		wg.Done()
 		return nil
@@ -133,9 +62,7 @@ func TestPodSidecarLabelChangeTriggersAddIptablesAction(t *testing.T) {
 	defer patches2.Reset()
 
 	patches2.ApplyFunc(deleteIptables, func(ns string) error {
-		mu.Lock()
-		defer mu.Unlock()
-		disabled = true
+		disabled.Store(true)
 		// Signal that addIptables has been called
 		wg.Done()
 		return nil
@@ -153,12 +80,33 @@ func TestPodSidecarLabelChangeTriggersAddIptablesAction(t *testing.T) {
 	_, err = client.CoreV1().Namespaces().Create(context.TODO(), namespace, metav1.CreateOptions{})
 	require.NoError(t, err)
 
-	pod := &corev1.Pod{
+	podWithBypassButNoSidecar := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-pod-no-sidecar",
+			Namespace: namespaceName,
+			Labels: map[string]string{
+				ByPassLabel: ByPassValue,
+			},
+		},
+		Spec: corev1.PodSpec{
+			NodeName: "test-node",
+		},
+	}
+
+	_, err = client.CoreV1().Pods(namespaceName).Create(context.TODO(), podWithBypassButNoSidecar, metav1.CreateOptions{})
+	assert.NoError(t, err)
+	assert.Equal(t, false, enabled.Load(), "unexpected value for enabled flag")
+	assert.Equal(t, false, disabled.Load(), "unexpected value for disabled flag")
+
+	podWithBypass := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "test-pod",
 			Namespace: namespaceName,
 			Labels: map[string]string{
-				"kmesh.net/bypass": "enabled",
+				ByPassLabel: ByPassValue,
+			},
+			Annotations: map[string]string{
+				annotation.SidecarStatus.Name: "placeholder",
 			},
 		},
 		Spec: corev1.PodSpec{
@@ -167,21 +115,34 @@ func TestPodSidecarLabelChangeTriggersAddIptablesAction(t *testing.T) {
 	}
 
 	wg.Add(1)
-	_, err = client.CoreV1().Pods(namespaceName).Create(context.TODO(), pod, metav1.CreateOptions{})
+	_, err = client.CoreV1().Pods(namespaceName).Create(context.TODO(), podWithBypass, metav1.CreateOptions{})
 	assert.NoError(t, err)
-
 	wg.Wait()
-	assert.Equal(t, true, enabled, "unexpected value for enabled flag")
+	assert.Equal(t, true, enabled.Load(), "unexpected value for enabled flag")
+	assert.Equal(t, false, disabled.Load(), "unexpected value for disabled flag")
 
-	enabled = false
-	disabled = false
+	enabled.Store(false)
+	disabled.Store(false)
 
-	newPod := pod.DeepCopy()
-	delete(newPod.Labels, "kmesh.net/bypass")
+	// Update pod by removing the bypass label
+	newPod := podWithBypass.DeepCopy()
+	delete(newPod.Labels, ByPassLabel)
 	wg.Add(1)
 	_, err = client.CoreV1().Pods(namespaceName).Update(context.TODO(), newPod, metav1.UpdateOptions{})
 	assert.NoError(t, err)
-
 	wg.Wait()
-	assert.Equal(t, true, disabled, "unexpected value for enabled flag")
+	assert.Equal(t, false, enabled.Load(), "unexpected value for disabled flag")
+	assert.Equal(t, true, disabled.Load(), "unexpected value for disabled flag")
+
+	enabled.Store(false)
+	disabled.Store(false)
+	// Update pod by adding the bypass label
+	newPod = podWithBypass.DeepCopy()
+	newPod.Labels[ByPassLabel] = ByPassValue
+	wg.Add(1)
+	_, err = client.CoreV1().Pods(namespaceName).Update(context.TODO(), newPod, metav1.UpdateOptions{})
+	assert.NoError(t, err)
+	wg.Wait()
+	assert.Equal(t, true, enabled.Load(), "unexpected value for enabled flag")
+	assert.Equal(t, false, disabled.Load(), "unexpected value for disabled flag")
 }
