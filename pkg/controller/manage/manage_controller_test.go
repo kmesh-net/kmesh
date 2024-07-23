661e1644dc5aae9c6e62b34608182b7ba6d89beb
Zhonghu Xu <xuzhonghu@huawei.com>
2024-07-23 10:27:57 +0800

    Add manage controller ut
    Signed-off-by: Zhonghu Xu <xuzhonghu@huawei.com>

--- pkg/controller/manage/manage_controller_test.go
+++ pkg/controller/manage/manage_controller_test.go
@@ -18,33 +18,41 @@ package kmeshmanage
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"reflect"
-	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/agiledragon/gomonkey/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"istio.io/istio/pkg/test/util/retry"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/tools/cache"
 
 	"kmesh.net/kmesh/pkg/constants"
-	kmeshsecurity "kmesh.net/kmesh/pkg/controller/security"
 	"kmesh.net/kmesh/pkg/utils"
 )
 
-func waitAndCheckManageAction(t *testing.T, wg *sync.WaitGroup, mu *sync.Mutex, enabled *bool, disabled *bool, enableExpected bool, disableExpected bool) {
-	wg.Wait()
-	mu.Lock()
-	defer mu.Unlock()
-	assert.Equal(t, enableExpected, *enabled, "unexpected value for enabled flag")
-	assert.Equal(t, disableExpected, *disabled, "unexpected value for disabled flag")
+func waitAndCheckManageAction(t *testing.T, enabled *atomic.Bool, disabled *atomic.Bool, enableExpected bool, disableExpected bool) {
+
+	retry.UntilSuccess(func() error {
+		// Wait for the handleKmeshManage to be called
+		if enableExpected != enabled.Load() || disableExpected != disabled.Load() {
+			return fmt.Errorf("enabled: %v, disabled: %v", enabled.Load(), disabled.Load())
+		}
+		return nil
+	})
+	assert.Equal(t, enableExpected, enabled.Load(), "unexpected value for enabled flag")
+	assert.Equal(t, disableExpected, disabled.Load(), "unexpected value for disabled flag")
 }
 
-func TestPodWithLabelChangeTriggersManageAction(t *testing.T) {
+func TestHandleKmeshManage(t *testing.T) {
 	client := fake.NewSimpleClientset()
 
 	err := os.Setenv("NODE_NAME", "test_node")
@@ -61,172 +69,261 @@ func TestPodWithLabelChangeTriggersManageAction(t *testing.T) {
 	defer close(stopChan)
 
 	go controller.Run(stopChan)
-	cache.WaitForCacheSync(stopChan, controller.podInformer.HasSynced)
+	cache.WaitForCacheSync(stopChan, controller.podInformer.HasSynced, controller.namespaceInformer.HasSynced)
 
-	var mu sync.Mutex
-	enabled := false
-	disabled := false
-
-	// Create a WaitGroup to synchronize the test
-	var wg sync.WaitGroup
+	enabled := atomic.Bool{}
+	disabled := atomic.Bool{}
 
 	patches := gomonkey.NewPatches()
 	defer patches.Reset()
-
 	patches.ApplyFunc(utils.HandleKmeshManage, func(ns string, op bool) error {
-		mu.Lock()
-		defer mu.Unlock()
 		if op {
-			enabled = true
+			enabled.Store(true)
 		} else {
-			disabled = true
+			disabled.Store(true)
 		}
-		// Signal that handleKmeshManage has been called
-		wg.Done()
 		return nil
 	})
 
-	pod := &corev1.Pod{
+	patches.ApplyMethodFunc(reflect.TypeOf(controller.queue), "AddRateLimited", func(item interface{}) {
+		queueItem, ok := item.(QueueItem)
+		if !ok {
+			t.Logf("expected QueueItem but got %T", item)
+			return
+		}
+		t.Logf("---------handle pod %s/%s annotation action %s", queueItem.podNs, queueItem.podName, queueItem.action)
+		pod, err := controller.podLister.Pods(queueItem.podNs).Get(queueItem.podName)
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				t.Logf("pod %s/%s has been deleted", queueItem.podNs, queueItem.podName)
+				return
+			}
+			t.Errorf("failed to get pod %s/%s: %v", queueItem.podNs, queueItem.podName, err)
+		}
+
+		if pod != nil {
+			namespace, _ := controller.namespaceLister.Get(pod.Namespace)
+			if queueItem.action == ActionAddAnnotation && utils.ShouldEnroll(pod, namespace) {
+				t.Logf("add annotation for pod %s/%s", pod.Namespace, pod.Name)
+				err = addKmeshAnnotation(controller.client, pod)
+			} else if queueItem.action == ActionDeleteAnnotation && !utils.ShouldEnroll(pod, namespace) {
+				t.Logf("delete annotation for pod %s/%s", pod.Namespace, pod.Name)
+				err = delKmeshAnnotation(controller.client, pod)
+			}
+		}
+
+		if err != nil {
+			t.Errorf("failed to handle pod %s/%s: %v", queueItem.podNs, queueItem.podName, err)
+		}
+	})
+
+	podWithoutLabel := &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-pod",
 			Namespace: "default",
+			Name:      "ut-pod",
+		},
+		Spec: corev1.PodSpec{
+			NodeName: "test-node",
+		},
+	}
+	podWithLabel := &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "ut-pod",
 			Labels:    map[string]string{constants.DataPlaneModeLabel: constants.DataPlaneModeKmesh},
 		},
 		Spec: corev1.PodSpec{
 			NodeName: "test-node",
 		},
 	}
-
-	wg.Add(1)
-	_, err = client.CoreV1().Pods("default").Create(context.TODO(), pod, metav1.CreateOptions{})
-	assert.NoError(t, err)
-
-	waitAndCheckManageAction(t, &wg, &mu, &enabled, &disabled, true, false)
-
-	enabled = false
-	disabled = false
-
-	delete(pod.Labels, constants.DataPlaneModeLabel)
-	wg.Add(1)
-	_, err = client.CoreV1().Pods("default").Update(context.TODO(), pod, metav1.UpdateOptions{})
-	assert.NoError(t, err)
-
-	waitAndCheckManageAction(t, &wg, &mu, &enabled, &disabled, false, true)
-}
-
-func TestPodWithoutLabelTriggersManageAction(t *testing.T) {
-	client := fake.NewSimpleClientset()
-
-	err := os.Setenv("NODE_NAME", "test_node")
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		os.Unsetenv("NODE_NAME")
-	})
-	controller, err := NewKmeshManageController(client, nil)
-	if err != nil {
-		t.Fatalf("error creating KmeshManageController: %v", err)
-	}
-
-	stopChan := make(chan struct{})
-	defer close(stopChan)
-
-	go controller.Run(stopChan)
-	cache.WaitForCacheSync(stopChan, controller.podInformer.HasSynced)
-
-	var mu sync.Mutex
-	enabled := false
-	disabled := false
-
-	// Create a WaitGroup to synchronize the test
-	var wg sync.WaitGroup
-
-	patches := gomonkey.NewPatches()
-	defer patches.Reset()
-
-	patches.ApplyFunc(utils.HandleKmeshManage, func(ns string, op bool) error {
-		mu.Lock()
-		defer mu.Unlock()
-		if op {
-			enabled = true
-		} else {
-			disabled = true
-		}
-		// Signal that handleKmeshManage has been called
-		wg.Done()
-		return nil
-	})
-
-	pod := &corev1.Pod{
+	podWithNoneLabel := &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-pod",
 			Namespace: "default",
-			Labels:    map[string]string{},
+			Name:      "ut-pod",
+			Labels:    map[string]string{constants.DataPlaneModeLabel: "none"},
 		},
 		Spec: corev1.PodSpec{
 			NodeName: "test-node",
 		},
 	}
 
-	_, err = client.CoreV1().Pods("default").Create(context.TODO(), pod, metav1.CreateOptions{})
-	assert.NoError(t, err)
-
-	enabled = false
-	disabled = false
-
-	pod.Labels[constants.DataPlaneModeLabel] = constants.DataPlaneModeKmesh
-	wg.Add(1)
-	_, err = client.CoreV1().Pods("default").Update(context.TODO(), pod, metav1.UpdateOptions{})
-	assert.NoError(t, err)
-
-	waitAndCheckManageAction(t, &wg, &mu, &enabled, &disabled, true, false)
-}
-
-func TestPodDeleteTriggersManageAction(t *testing.T) {
-	isSend := false
-	// Create a WaitGroup to synchronize the test
-	var wg sync.WaitGroup
-	client := fake.NewSimpleClientset()
-
-	err := os.Setenv("NODE_NAME", "test_node")
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		os.Unsetenv("NODE_NAME")
-	})
-
-	SecretManager := &kmeshsecurity.SecretManager{}
-	patches1 := gomonkey.NewPatches()
-	patches1.ApplyMethod(reflect.TypeOf(SecretManager), "SendCertRequest", func(_ *kmeshsecurity.SecretManager, identity string, op int) {
-		isSend = true
-		wg.Done()
-	})
-	defer patches1.Reset()
-
-	controller, err := NewKmeshManageController(client, SecretManager)
-	if err != nil {
-		t.Fatalf("error creating KmeshManageController: %v", err)
-	}
-
-	stopChan := make(chan struct{})
-	defer close(stopChan)
-
-	go controller.Run(stopChan)
-	cache.WaitForCacheSync(stopChan, controller.podInformer.HasSynced)
-
-	pod := &corev1.Pod{
+	nsWithoutLabel := &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Namespace",
+			APIVersion: "v1",
+		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-pod",
-			Namespace: "default",
-			Labels:    map[string]string{constants.DataPlaneModeLabel: constants.DataPlaneModeKmesh},
-		},
-		Spec: corev1.PodSpec{
-			NodeName: "test-node",
+			Name: "default",
 		},
 	}
-	wg.Add(1)
-	_, err = client.CoreV1().Pods("default").Create(context.TODO(), pod, metav1.CreateOptions{})
+	nsWithLabel := &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Namespace",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "default",
+			Labels: map[string]string{constants.DataPlaneModeLabel: constants.DataPlaneModeKmesh},
+		},
+	}
+
+	type args struct {
+		namespace              *corev1.Namespace
+		pod                    *corev1.Pod
+		create, update, delete bool
+	}
+	tests := []struct {
+		name             string
+		args             args
+		expectManaged    bool
+		expectDisManaged bool
+	}{
+		{
+			name: "1. ns without label, pod without label",
+			args: args{
+				namespace: nsWithoutLabel,
+				pod:       podWithoutLabel,
+				create:    true,
+			},
+			expectManaged:    false,
+			expectDisManaged: false,
+		},
+		{
+			name: "2. ns without label, pod update with label",
+			args: args{
+				namespace: nsWithoutLabel,
+				pod:       podWithLabel,
+				update:    true,
+			},
+			expectManaged:    true,
+			expectDisManaged: false,
+		},
+		{
+			name: "2.1 ns without label, pod update with `none` label",
+			args: args{
+				namespace: nsWithoutLabel,
+				pod:       podWithNoneLabel,
+				update:    true,
+			},
+			expectManaged:    false,
+			expectDisManaged: true,
+		},
+		{
+			name: "3. ns without label, pod with none label delete",
+			args: args{
+				namespace: nsWithoutLabel,
+				pod:       podWithNoneLabel,
+				delete:    true,
+			},
+			expectManaged:    false,
+			expectDisManaged: false,
+		},
+		{
+			name: "4. ns without label, pod with label",
+			args: args{
+				namespace: nsWithoutLabel,
+				pod:       podWithLabel,
+				create:    true,
+			},
+			expectManaged: true,
+		},
+		{
+			name: "4.1 ns without label, pod with label delete",
+			args: args{
+				namespace: nsWithoutLabel,
+				pod:       podWithLabel,
+				delete:    true,
+			},
+			expectManaged:    false,
+			expectDisManaged: false,
+		},
+
+		{
+			name: "5. ns with label, pod without label",
+			args: args{
+				namespace: nsWithLabel,
+				pod:       podWithoutLabel,
+				create:    true,
+			},
+			expectManaged:    true,
+			expectDisManaged: false,
+		},
+		{
+			name: "6. ns with label, pod update with label",
+			args: args{
+				namespace: nsWithLabel,
+				pod:       podWithLabel,
+				update:    true,
+			},
+			expectManaged:    false,
+			expectDisManaged: false,
+		},
+		{
+			name: "7. ns with label, pod update with none label",
+			args: args{
+				namespace: nsWithLabel,
+				pod:       podWithNoneLabel,
+				update:    true,
+			},
+			expectManaged:    false,
+			expectDisManaged: true,
+		},
+		{
+			name: "8. ns with label, pod delete with none label",
+			args: args{
+				namespace: nsWithLabel,
+				pod:       podWithNoneLabel,
+				delete:    true,
+			},
+			expectDisManaged: false,
+		},
+	}
+
+	_, err = client.CoreV1().Namespaces().Create(context.TODO(), nsWithoutLabel, metav1.CreateOptions{})
 	assert.NoError(t, err)
-	err = client.CoreV1().Pods("default").Delete(context.TODO(), pod.Name, metav1.DeleteOptions{})
-	assert.NoError(t, err)
-	wg.Wait()
-	assert.Equal(t, true, isSend, "unexpected value for enabled flag")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err = client.CoreV1().Namespaces().Update(context.TODO(), tt.args.namespace, metav1.UpdateOptions{})
+			assert.NoError(t, err)
+			// TODO: find a way to wait for namespace informer to sync
+			time.Sleep(5 * time.Millisecond)
+
+			enabled.Store(false)
+			disabled.Store(false)
+
+			if tt.args.create {
+				pod := tt.args.pod.DeepCopy()
+				_, err = client.CoreV1().Pods(tt.args.namespace.Name).Create(context.TODO(), pod, metav1.CreateOptions{})
+				assert.NoError(t, err)
+			}
+
+			if tt.args.update {
+				pod, _ := client.CoreV1().Pods(tt.args.namespace.Name).Get(context.TODO(), tt.args.pod.Name, metav1.GetOptions{})
+				if pod != nil {
+					pod.Labels = tt.args.pod.Labels
+				}
+				_, err = client.CoreV1().Pods(tt.args.namespace.Name).Update(context.TODO(), pod, metav1.UpdateOptions{})
+				assert.NoError(t, err)
+			}
+
+			if tt.args.delete {
+				err = client.CoreV1().Pods(tt.args.namespace.Name).Delete(context.TODO(), tt.args.pod.Name, metav1.DeleteOptions{})
+				assert.NoError(t, err)
+			}
+
+			waitAndCheckManageAction(t, &enabled, &disabled, tt.expectManaged, tt.expectDisManaged)
+		})
+	}
 }
