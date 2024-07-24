527f0e8db5dff41306f4270817b00793e9c9795d
superCharge-xsy <xiesongyang@huawei.com>
2024-07-24 11:35:57 +0800

    set innerMap
    Signed-off-by: superCharge-xsy <xiesongyang@huawei.com>

--- pkg/bpf/bpf_kmesh_workload.go
+++ pkg/bpf/bpf_kmesh_workload.go
@@ -76,6 +76,7 @@ func (sc *BpfSockConnWorkload) loadKmeshSockConnObjects() (*ebpf.CollectionSpec,
 		return nil, err
 	}
 
+	SetInnerMap(spec)
 	setMapPinType(spec, ebpf.PinByName)
 	if err = spec.LoadAndAssign(&sc.KmeshCgroupSockWorkloadObjects, &opts); err != nil {
 		return nil, err
@@ -258,6 +259,7 @@ func (so *BpfSockOpsWorkload) loadKmeshSockopsObjects() (*ebpf.CollectionSpec, e
 		return nil, fmt.Errorf("error: loadKmeshSockopsObjects() spec is nil")
 	}
 
+	SetInnerMap(spec)
 	setMapPinType(spec, ebpf.PinByName)
 	if err = spec.LoadAndAssign(&so.KmeshSockopsWorkloadObjects, &opts); err != nil {
 		return nil, err
