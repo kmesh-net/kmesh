932e01258623af5462c112d53beff5bb1db1a9a8
Zhonghu Xu <xuzhonghu@huawei.com>
2024-07-29 16:51:01 +0800

    Donot start up kmesh manage controller when bypass enabled
    Signed-off-by: Zhonghu Xu <xuzhonghu@huawei.com>

--- pkg/utils/enroll.go
+++ pkg/utils/enroll.go
@@ -105,6 +105,7 @@ func PatchKmeshRedirectAnnotation(client kubernetes.Interface, pod *corev1.Pod) 
 		log.Debugf("Pod %s in namespace %s already has annotation %s", pod.Name, pod.Namespace, constants.KmeshRedirectionAnnotation)
 		return nil
 	}
+	log.Infof("add %s annotation for pod %s/%s", constants.KmeshRedirectionAnnotation, pod.Namespace, pod.Name)
 	_, err := client.CoreV1().Pods(pod.Namespace).Patch(
 		context.Background(),
 		pod.Name,
@@ -120,6 +121,7 @@ func DelKmeshRedirectAnnotation(client kubernetes.Interface, pod *corev1.Pod) er
 		log.Debugf("Pod %s in namespace %s does not have annotation %s", pod.Name, pod.Namespace, constants.KmeshRedirectionAnnotation)
 		return nil
 	}
+	log.Infof("delete %s annotation for pod %s/%s", constants.KmeshRedirectionAnnotation, pod.Namespace, pod.Name)
 	_, err := client.CoreV1().Pods(pod.Namespace).Patch(
 		context.Background(),
 		pod.Name,
