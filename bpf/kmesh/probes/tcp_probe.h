f03d7653e00d3d5c77c3b06aed9ef91d56397b5d
wuchangye <wuchangye@huawei.com>
2024-07-29 18:31:23 +0800

    Add the observation information for tcp link
    Signed-off-by: wuchangye <wuchangye@huawei.com>

--- bpf/kmesh/probes/tcp_probe.h
+++ bpf/kmesh/probes/tcp_probe.h
@@ -20,7 +20,18 @@ struct tcp_probe_info {
     __u32 sent_bytes;
     __u32 received_bytes;
     __u32 conn_success;
-    __u32 state;
+    __u32 state;   /* tcp state */
+    __u32 srtt_us; /* smoothed round trip time << 3 in usecs */
+    __u32 rtt_min;
+    __u32 mss_cache;     /* Cached effective mss, not including SACKS */
+    __u32 total_retrans; /* Total retransmits for entire connection */
+    __u32 segs_in;       /* RFC4898 tcpEStatsPerfSegsIn
+                          * total number of segments in.
+                          */
+    __u32 segs_out;      /* RFC4898 tcpEStatsPerfSegsOut
+                          * The total number of segments sent.
+                          */
+    __u32 lost_out;      /* Lost packets			*/
 };
 
 struct {
@@ -44,6 +55,20 @@ static inline void constuct_tuple(struct bpf_sock *sk, struct bpf_sock_tuple *tu
     return;
 }
 
+static inline void get_tcp_probe_info(struct bpf_tcp_sock *tcp_sock, struct tcp_probe_info *info)
+{
+    info->sent_bytes = tcp_sock->delivered;
+    info->received_bytes = tcp_sock->bytes_received;
+    info->srtt_us = tcp_sock->srtt_us;
+    info->rtt_min = tcp_sock->rtt_min;
+    info->mss_cache = tcp_sock->mss_cache;
+    info->total_retrans = tcp_sock->total_retrans;
+    info->segs_in = tcp_sock->segs_in;
+    info->segs_out = tcp_sock->segs_out;
+    info->lost_out = tcp_sock->lost_out;
+    return;
+}
+
 static inline void
 tcp_report(struct bpf_sock *sk, struct bpf_tcp_sock *tcp_sock, struct sock_storage_data *storage, __u32 state)
 {
@@ -63,9 +88,8 @@ tcp_report(struct bpf_sock *sk, struct bpf_tcp_sock *tcp_sock, struct sock_stora
         info->close_ns = bpf_ktime_get_ns();
         info->duration = info->close_ns - storage->connect_ns;
     }
-    info->sent_bytes = tcp_sock->delivered;
-    info->received_bytes = tcp_sock->bytes_received;
     info->conn_success = storage->connect_success;
+    get_tcp_probe_info(tcp_sock, info);
 
     bpf_ringbuf_submit(info, 0);
 }
