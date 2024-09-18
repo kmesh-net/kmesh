5730dff54fdd4af15ebd5f4ef21b92d70780b03b
YaoZengzeng <yaozengzeng@huawei.com>
2024-09-18 02:59:30 +0000

    port forward
    Signed-off-by: YaoZengzeng <yaozengzeng@huawei.com>
    Signed-off-by: adam <wangjiabao@harmonycloud.cn>

--- ctl/main.go
+++ ctl/main.go
@@ -17,8 +17,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 	"istio.io/istio/pkg/kube"
@@ -52,7 +56,47 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("kubecli.Revision() is %s", kubecli.Revision())
+	podName := "kmesh-7jz9n"
+	podNamespace := "kmesh-system"
+
+	fw, err := kubecli.NewPortForwarder(podName, podNamespace, "", 0, 15200)
+	if err != nil {
+		fmt.Printf("failed to create port forwarder: %v\n", err)
+		os.Exit(1)
+	}
+	if err := fw.Start(); err != nil {
+		fmt.Printf("failed to start port forwarder: %v\n", err)
+		os.Exit(1)
+	}
+	defer fw.Close()
+
+	method := "GET"
+	req, err := http.NewRequest(method, fmt.Sprintf("http://%s/%s", fw.Address(), "/debug/loggers?name=default"), nil)
+	if err != nil {
+		fmt.Printf("failed to create http request: %v\n", err)
+		os.Exit(1)
+	}
+	resp, err := (&http.Client{
+		Timeout: time.Second * 15,
+	}).Do(req.WithContext(context.Background()))
+	if err != nil {
+		fmt.Printf("failed to do http request: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("http status is %v which is not %v\n", resp.StatusCode, http.StatusOK)
+		os.Exit(1)
+	}
+
+	out, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("failed to io.ReadAll: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("response body is %s", string(out))
 
 	rootCmd.AddCommand(logcmd.NewCmd())
 
