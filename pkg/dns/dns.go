3e4ee9948d3168eb1a3334ae9a2332e39ccbc213
adam <wangjiabao@harmonycloud.cn>
2024-09-21 21:29:05 +0800

    use depguard to prohibitate importing github.com/golang/protobuf
    Signed-off-by: adam <wangjiabao@harmonycloud.cn>

--- pkg/dns/dns.go
+++ pkg/dns/dns.go
@@ -28,7 +28,6 @@ import (
 	clusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	endpointv3 "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
-	//"github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/miekg/dns"
