97e282a3cd3bbdf7f595b09099abfbbb84590a3b
thebigbone <pacman@duck.com>
2024-10-25 12:20:20 +0530

    use utils for applying yaml
    Signed-off-by: thebigbone <pacman@duck.com>

--- ctl/install/install.go
+++ ctl/install/install.go
@@ -17,123 +17,46 @@
 package install
 
 import (
-	"bufio"
-	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
-	"k8s.io/apimachinery/pkg/types"
-	utilyaml "k8s.io/apimachinery/pkg/util/yaml"
-	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/discovery/cached/memory"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/restmapper"
-	"k8s.io/client-go/tools/clientcmd"
+
+	"kmesh.net/kmesh/ctl/utils"
 )
 
-type Resource struct {
-	Name string
-	URL  string
-}
-
-var decUnstructured = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-
-func doServerSideApply(ctx context.Context, cfg *rest.Config, obj *unstructured.Unstructured) error {
-	dc, err := discovery.NewDiscoveryClientForConfig(cfg)
-	if err != nil {
-		return err
-	}
-	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
-
-	dyn, err := dynamic.NewForConfig(cfg)
-	if err != nil {
-		return err
-	}
-
-	mapping, err := mapper.RESTMapping(obj.GroupVersionKind().GroupKind(), obj.GroupVersionKind().Version)
-	if err != nil {
-		return err
-	}
-
-	var dr dynamic.ResourceInterface
-	if mapping.Scope.Name() == meta.RESTScopeNameNamespace {
-		dr = dyn.Resource(mapping.Resource).Namespace(obj.GetNamespace())
-	} else {
-		dr = dyn.Resource(mapping.Resource)
-	}
-
-	data, err := json.Marshal(obj)
-	if err != nil {
-		return err
-	}
-
-	_, err = dr.Patch(ctx, obj.GetName(), types.ApplyPatchType, data, metav1.PatchOptions{
-		FieldManager: "sample-controller",
-	})
-
-	return err
+type GitHubFile struct {
+	Name        string `json:"name"`
+	DownloadURL string `json:"download_url"`
 }
 
 func NewCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "install",
 		Short: "install kmesh with all the resources",
-		Example: `# Install kmesh without version argument (defaults to main):
-kmeshctl install
-
-# Install a specific version
-kmeshctl install --version <version>`,
 		Run: func(cmd *cobra.Command, args []string) {
 			version, err := cmd.Flags().GetString("version")
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			kubeconfig := os.Getenv("HOME") + "/.kube/config"
-
-			config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+			cli, err := utils.CreateKubeClient()
 			if err != nil {
-				panic(err.Error())
-			}
-
-			resources := []Resource{
-				{Name: "clusterRole", URL: getURL(version, "clusterrole.yaml")},
-				{Name: "clusterRoleBinding", URL: getURL(version, "clusterrolebinding.yaml")},
-				{Name: "kmesh", URL: getURL(version, "kmesh.yaml")},
-				{Name: "l7EnvoyFilter", URL: getURL(version, "l7-envoyfilter.yaml")},
-				{Name: "serviceAccount", URL: getURL(version, "serviceaccount.yaml")},
+				log.Fatalf("failed to create cli client: %v", err)
+				os.Exit(1)
 			}
 
 			fmt.Println("install kmesh version: ", version)
 
-			for _, resource := range resources {
-				resp, err := getURLResponse(resource.URL)
-				if err != nil {
-					log.Fatalf("Error fetching %s: %v", resource.URL, err)
-				}
-				defer resp.Body.Close()
-
-				deployYaml, err := io.ReadAll(resp.Body)
-				if err != nil {
-					log.Printf("yamlFile.Get err   #%v ", err)
-				}
-
-				fmt.Println("initializing ", resource.Name)
-				err = applyYaml(deployYaml, config)
-				if err != nil {
-					log.Fatal(err)
-				}
+			combinedYAMLFile := getYAMLFile()
+			err = cli.ApplyYAMLContents("", combinedYAMLFile)
+			if err != nil {
+				log.Fatal(err)
 			}
 		},
 	}
@@ -142,44 +65,39 @@ kmeshctl install --version <version>`,
 	return cmd
 }
 
-func applyYaml(yamlFile []byte, config *rest.Config) error {
-	multidocReader := utilyaml.NewYAMLReader(bufio.NewReader(bytes.NewReader(yamlFile)))
+func getYAMLFile() string {
+	url := fmt.Sprintf("https://api.github.com/repos/kmesh-net/kmesh/contents/deploy/yaml?ref=main")
 
-	for {
-		buf, err := multidocReader.Read()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return err
-		}
-
-		obj := &unstructured.Unstructured{}
-		_, _, err = decUnstructured.Decode(buf, nil, obj)
-		if err != nil {
-			return err
-		}
-
-		err = doServerSideApply(context.TODO(), config, obj)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func getURL(version, filename string) string {
-	if version != "main" {
-		return "https://raw.githubusercontent.com/kmesh-net/kmesh/refs/heads/release-" + version + "/deploy/yaml/" + filename
-	}
-	return "https://raw.githubusercontent.com/kmesh-net/kmesh/refs/heads/" + version + "/deploy/yaml/" + filename
-}
-
-func getURLResponse(url string) (*http.Response, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		log.Fatal("error fetching files:", err)
 	}
-	return resp, nil
+	defer resp.Body.Close()
+
+	var files []GitHubFile
+	err = json.NewDecoder(resp.Body).Decode(&files)
+	if err != nil {
+		log.Fatal("error decoding JSON:", err)
+	}
+
+	var combinedYAML string
+
+	for _, file := range files {
+		if filepath.Ext(file.Name) == ".yaml" {
+			resp, err := http.Get(file.DownloadURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer resp.Body.Close()
+
+			deployYaml, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalf("yamlFile.Get err   #%v ", err)
+			}
+
+			combinedYAML += string(deployYaml) + "\n---\n"
+		}
+	}
+
+	return combinedYAML
 }
