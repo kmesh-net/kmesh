e2c14c56613e581816d58959d6af1c3c2e563a39
1640528278@qq.com <1640528278@qq.com>
2024-10-30 02:39:51 +0800

    ads mode support consistent hash
    Signed-off-by: 1640528278@qq.com <1640528278@qq.com>

--- bpf/kmesh/ads/include/kmesh_common.h
+++ bpf/kmesh/ads/include/kmesh_common.h
@@ -29,6 +29,11 @@
         val;                                                                                                           \
     })
 
+struct bpf_mem_ptr {
+    void *ptr;
+    __u32 size;
+};
+
 #if !ENHANCED_KERNEL
 static inline int bpf__strncmp(const char *dst, int n, const char *src)
 {
