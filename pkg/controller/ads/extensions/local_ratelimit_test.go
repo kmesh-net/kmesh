f9d75834e00d92c6d2c8b6671689707bc4b857db
yuan <yuanqijing00x@gmail.com>
2024-11-01 16:30:03 +0800

    ut: fix local ratelimit code cause other faile.
    Signed-off-by: yuan <yuanqijing00x@gmail.com>

--- pkg/controller/ads/extensions/local_ratelimit_test.go
+++ pkg/controller/ads/extensions/local_ratelimit_test.go
@@ -1,3 +1,6 @@
+//go:build feature_ratelimit
+// +build feature_ratelimit
+
 /*
  * Copyright The Kmesh Authors.
  *
@@ -13,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package extensions
 
 import (
