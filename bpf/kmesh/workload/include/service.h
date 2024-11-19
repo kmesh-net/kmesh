ef419a95cb4e12165c4c7787c952f8b85be3e043
wuchangye <wuchangye@huawei.com>
2024-11-19 22:14:08 +0800

    optimize: fix bpf ins over 100w
    Signed-off-by: wuchangye <wuchangye@huawei.com>

--- bpf/kmesh/workload/include/service.h
+++ bpf/kmesh/workload/include/service.h
@@ -44,53 +44,57 @@ static inline int lb_random_handle(struct kmesh_context *kmesh_ctx, __u32 servic
     return 0;
 }
 
-static inline int
-lb_locality_failover_handle(struct kmesh_context *kmesh_ctx, __u32 service_id, service_value *service_v, bool is_strict)
+static inline int lb_locality_strict_handle(struct kmesh_context *kmesh_ctx, __u32 service_id, service_value *service_v)
 {
-    int ret = 0;
-    uint32_t rand_k = 0;
+    int ret = -ENOENT;
     endpoint_key endpoint_k = {0};
     endpoint_value *endpoint_v = NULL;
     endpoint_k.service_id = service_id;
-    struct ip_addr zero_addr = {0};
-    __u32 zero_port = 0;
+
+    if (service_v->prio_endpoint_count[0]) {
+        endpoint_k.backend_index = bpf_get_prandom_u32() % service_v->prio_endpoint_count[0] + 1;
+        endpoint_v = map_lookup_endpoint(&endpoint_k);
+        if (endpoint_v)
+            ret = endpoint_manager(kmesh_ctx, endpoint_v, service_id, service_v);
+    }
+
+    if (ret) {
+        kmesh_ctx->dnat_ip = (struct ip_addr){0};
+        kmesh_ctx->dnat_port = 0;
+        BPF_LOG(ERR, SERVICE, "locality loadbalance match nothing in STRICT mode, ret:%d\n", ret);
+    }
+    return ret;
+}
+
+static inline int
+lb_locality_failover_handle(struct kmesh_context *kmesh_ctx, __u32 service_id, service_value *service_v)
+{
+    int i, ret = -ENOENT;
+    endpoint_key endpoint_k = {0};
+    endpoint_value *endpoint_v = NULL;
+    endpoint_k.service_id = service_id;
 
     // #pragma unroll
-    for (int match_prio = 0; match_prio < PRIO_COUNT; match_prio++) {
-        endpoint_k.prio = match_prio; // 0->6
-        // if we have endpoints in this prio
-        if (service_v->prio_endpoint_count[match_prio] > 0) {
-            rand_k = bpf_get_prandom_u32() % service_v->prio_endpoint_count[match_prio] + 1;
-            endpoint_k.backend_index = rand_k;
-            endpoint_v = map_lookup_endpoint(&endpoint_k);
-            if (!endpoint_v) {
-                BPF_LOG(
-                    ERR, SERVICE, "find endpoint [%u/%u/%u] failed", service_id, match_prio, endpoint_k.backend_index);
-                return -ENOENT;
-            }
-            ret = endpoint_manager(kmesh_ctx, endpoint_v, service_id, service_v);
-            if (ret != 0) {
-                if (ret != -ENOENT)
-                    BPF_LOG(ERR, SERVICE, "endpoint_manager failed, ret:%d\n", ret);
-                return ret;
-            }
-            BPF_LOG(
-                DEBUG,
-                SERVICE,
-                "locality lb matched backend_uid %d with priority %d\n",
-                endpoint_v->backend_uid,
-                match_prio);
-            return 0; // find the backend successfully
+    for (i = 0; i < PRIO_COUNT; i++) {
+        if (service_v->prio_endpoint_count[i] == 0)
+            continue;
+
+        endpoint_k.prio = i;
+        endpoint_k.backend_index = bpf_get_prandom_u32() % service_v->prio_endpoint_count[i] + 1;
+        endpoint_v = map_lookup_endpoint(&endpoint_k);
+        if (!endpoint_v) {
+            ret = -ENOENT;
+            break;
         }
-        if (is_strict) { // only match max priority in strict mode
-            kmesh_ctx->dnat_ip = zero_addr;
-            kmesh_ctx->dnat_port = zero_port;
-            BPF_LOG(DEBUG, SERVICE, "locality loadbalance match nothing in STRICT mode\n");
-            return -ENOENT;
-        }
+
+        ret = endpoint_manager(kmesh_ctx, endpoint_v, service_id, service_v);
+        if (ret != -ENOENT)
+            break;
     }
-    // no backend matched
-    return -ENOENT;
+
+    if (ret)
+        BPF_LOG(ERR, SERVICE, "lb_locality_failover_hdl [%u %u %u]\n", service_id, i, endpoint_k.backend_index);
+    return ret;
 }
 
 static inline int service_manager(struct kmesh_context *kmesh_ctx, __u32 service_id, service_value *service_v)
@@ -118,10 +122,10 @@ static inline int service_manager(struct kmesh_context *kmesh_ctx, __u32 service
         ret = lb_random_handle(kmesh_ctx, service_id, service_v);
         break;
     case LB_POLICY_STRICT:
-        ret = lb_locality_failover_handle(kmesh_ctx, service_id, service_v, true);
+        ret = lb_locality_strict_handle(kmesh_ctx, service_id, service_v);
         break;
     case LB_POLICY_FAILOVER:
-        ret = lb_locality_failover_handle(kmesh_ctx, service_id, service_v, false);
+        ret = lb_locality_failover_handle(kmesh_ctx, service_id, service_v);
         break;
     default:
         BPF_LOG(ERR, SERVICE, "unsupported load balance type:%u\n", service_v->lb_policy);
