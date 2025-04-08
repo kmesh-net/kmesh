5ccca911a72fd92a22e75b85fc5df3c11321a41f
Yash Patel <yp969803@gmail.com>
2025-04-09 00:40:01 +0530

    feat: added recv_msg_prog
    Signed-off-by: Yash Patel <yp969803@gmail.com>
    
    rfac: bpf2go
    
    Signed-off-by: Yash Patel <yp969803@gmail.com>
    
    chore: run make-gen
    
    Signed-off-by: Yash Patel <yp969803@gmail.com>
    
    rfac: recv_msg_prog
    
    Signed-off-by: Yash Patel <yp969803@gmail.com>

---
+++ bpf/kmesh/workload/recvmsg.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Kmesh */
+
+#include <linux/bpf.h>
+#include <sys/socket.h>
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+#include "bpf_log.h"
+#include "encoder.h"
+
+SEC("sk_skb")
+int recvmsg_prog(struct __sk_buff *skb)
+{
+    if (skb->family != AF_INET && skb->family != AF_INET6)
+        return SK_PASS;
+
+    struct bpf_sock *sk = skb->sk;
+    if (!sk)
+        return SK_PASS;
+
+    __u32 siz = skb->len;
+    return SK_PASS;
+}
