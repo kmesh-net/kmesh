1eda40b6ce2b1164e714e5209579728b74a29ddf
ravjot07 <ravu2004@gmail.com>
2025-04-19 01:16:32 +0530

    test: added test for kmeshctl log and secret
    Signed-off-by: ravjot07 <ravu2004@gmail.com>

--- test/e2e/kmeshctl_test.go
+++ test/e2e/kmeshctl_test.go
@@ -21,11 +21,20 @@
 package kmesh
 
 import (
+	"bufio"
+	"context"
+	"crypto/rand"
+    "encoding/hex"
 	"encoding/json"
+	"fmt"
 	"os/exec"
 	"strings"
 	"testing"
 	"time"
+
+	v1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "kmesh.net/kmesh/ctl/utils"
 )
 
 func runVersionCmd(args ...string) (string, error) {
@@ -199,4 +208,181 @@ func TestKmeshctlDump(t *testing.T) {
 		}
 	})
  }
+ 
+ func verifyLogOutputHeaders(t *testing.T, output, expectedHeader string) {
+	scanner := bufio.NewScanner(strings.NewReader(output))
+	found := false
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if strings.Contains(line, expectedHeader) {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Errorf("Expected output to contain header %q but it did not. Full output:\n%s", expectedHeader, output)
+	}
+ }
+
+ func TestKmeshctlLog(t *testing.T) {
+	podName := findKmeshPod(t)
+	waitForPodRunning(t, podName)
+ 
+	t.Run("get-all-loggers", func(t *testing.T) {
+		output, err := getLogOutputs(podName)
+		if err != nil {
+			t.Fatalf("Failed to get logger names: %v, output: %s", err, output)
+		}
+		t.Logf("Output of 'kmeshctl log %s':\n%s", podName, output)
+		verifyLogOutputHeaders(t, output, "Existing Loggers:")
+	})
+ 
+	t.Run("get-default-logger-level", func(t *testing.T) {
+		output, err := getLogOutputs(podName, "default")
+		if err != nil {
+			t.Fatalf("Failed to get default logger level: %v, output: %s", err, output)
+		}
+		t.Logf("Output of 'kmeshctl log %s default':\n%s", podName, output)
+		if !strings.Contains(output, "Logger Name:") || !strings.Contains(output, "Logger Level:") {
+			t.Errorf("Expected output to contain 'Logger Name:' and 'Logger Level:', but got: %s", output)
+		}
+	})
+ 
+ 
+	t.Run("set-default-logger-level", func(t *testing.T) {
+		output, err := getLogOutputs(podName, "--set", "default:debug")
+		if err != nil {
+			t.Fatalf("Failed to set default logger level: %v, output: %s", err, output)
+		}
+		t.Logf("Output of 'kmeshctl log %s --set default:debug':\n%s", podName, output)
+		output2, err := getLogOutputs(podName, "default")
+		if err != nil {
+			t.Fatalf("Failed to get default logger level after setting: %v, output: %s", err, output2)
+		}
+		t.Logf("Output of 'kmeshctl log %s default' after setting:\n%s", podName, output2)
+		if !strings.Contains(strings.ToLower(output2), "debug") {
+			t.Errorf("Expected default logger level to be 'debug', but output was: %s", output2)
+		}
+	})
+ }
+ 
+ type IpSecKey struct {
+	AeadKeyName string `json:"AeadKeyName"`
+	AeadKey     []byte `json:"AeadKey"`
+	Length      int    `json:"Length"`
+	Spi         int    `json:"Spi"`
+ }
+ 
+ 
+ func waitForSecret(secretName, namespace string, timeout time.Duration) (*v1.Secret, error) {
+	clientset, err := utils.CreateKubeClient()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kube client: %v", err)
+	}
+ 
+ 
+	deadline := time.Now().Add(timeout)
+	for time.Now().Before(deadline) {
+		sec, err := clientset.Kube().CoreV1().Secrets(namespace).Get(context.TODO(), secretName, metav1.GetOptions{})
+		if err == nil {
+			return sec, nil
+		}
+		time.Sleep(2 * time.Second)
+	}
+	return nil, fmt.Errorf("timeout waiting for secret %q in namespace %q", secretName, namespace)
+ }
+ 
+ func deleteSecret(secretName, namespace string) error {
+	clientset, err := utils.CreateKubeClient()
+	if err != nil {
+		return fmt.Errorf("failed to create kube client: %v", err)
+	}
+	_ = clientset.Kube().CoreV1().Secrets(namespace).Delete(context.TODO(), secretName, metav1.DeleteOptions{})
+	return nil
+ }
+ 
+ func generateRandomKey() (string, error) {
+	keyBytes := make([]byte, 36)
+	_, err := rand.Read(keyBytes)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate random key: %v", err)
+	}
+	return hex.EncodeToString(keyBytes), nil
+ }
+ 
+ 
+ func TestKmeshctlSecret(t *testing.T) {
+	const secretName = "kmesh-ipsec"
+	const namespace = "kmesh-system"
+ 
+	_ = deleteSecret(secretName, namespace)
+	t.Log("Deleted existing secret (if any)")
+ 
+ 
+	key1, err := generateRandomKey()
+	if err != nil {
+		t.Fatalf("failed to generate random key: %v", err)
+	}
+	t.Logf("Generated key1: %s", key1)
+ 
+ 
+	cmd := exec.Command("kmeshctl", "secret", "--key", key1)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("failed to run kmeshctl secret command: %v, output: %s", err, string(output))
+	}
+	t.Logf("Output of first 'kmeshctl secret' command: %s", string(output))
+ 
+ 
+	sec, err := waitForSecret(secretName, namespace, 30*time.Second)
+	if err != nil {
+		t.Fatalf("failed to get created secret: %v", err)
+	}
+ 
+	dataB64, exists := sec.Data["ipSec"]
+	if !exists {
+		t.Fatalf("secret %q does not contain key 'ipSec'", secretName)
+	}
+
+	var ipSecKey IpSecKey
+	err = json.Unmarshal(dataB64, &ipSecKey)
+	if err != nil {
+		t.Fatalf("failed to unmarshal secret data: %v", err)
+	}
+	t.Logf("Created secret with SPI: %d", ipSecKey.Spi)
+	if ipSecKey.Spi != 1 {
+		t.Errorf("Expected SPI to be 1 on creation, got %d", ipSecKey.Spi)
+	}
+
+	key2, err := generateRandomKey()
+	if err != nil {
+		t.Fatalf("failed to generate second random key: %v", err)
+	}
+	t.Logf("Generated key2: %s", key2)
+	cmd = exec.Command("kmeshctl", "secret", "--key", key2)
+	output, err = cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("failed to run kmeshctl secret command for update: %v, output: %s", err, string(output))
+	}
+	t.Logf("Output of second 'kmeshctl secret' command: %s", string(output))
+ 
+
+	secUpdated, err := waitForSecret(secretName, namespace, 30*time.Second)
+	if err != nil {
+		t.Fatalf("failed to get updated secret: %v", err)
+	}
+	dataB64 = secUpdated.Data["ipSec"]
+	var ipSecKeyUpdated IpSecKey
+	err = json.Unmarshal(dataB64, &ipSecKeyUpdated)
+	if err != nil {
+		t.Fatalf("failed to unmarshal updated secret data: %v", err)
+	}
+	t.Logf("Updated secret with SPI: %d", ipSecKeyUpdated.Spi)
+	expectedSPI := ipSecKey.Spi + 1
+	if ipSecKeyUpdated.Spi != expectedSPI {
+		t.Errorf("Expected updated SPI to be %d, but got %d", expectedSPI, ipSecKeyUpdated.Spi)
+	}
+ }
+ 
+ 
  
