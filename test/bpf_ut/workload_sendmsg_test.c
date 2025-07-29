efb7114a6e6c07da86313f8b6ca2dc76c0247bd9
sweeywu <sweetwx6@gmail.com>
2025-07-29 20:42:14 +0800

    ut_sendmsg_ipv4_TLV_updata
    Signed-off-by: sweeywu <sweetwx6@gmail.com>

--- test/bpf_ut/workload_sendmsg_test.c
+++ test/bpf_ut/workload_sendmsg_test.c
@@ -2,7 +2,7 @@
 #include <bpf/bpf_helpers.h>
 #include "bpf_log.h"
 #include "bpf_common.h"
-struct sock_storage_data mock_storage = {
+struct sock_storage_data mock_storage_ipv4 = {
     .via_waypoint = 1,
     .has_encoded = 0,
     .sk_tuple = {
@@ -12,13 +12,28 @@ struct sock_storage_data mock_storage = {
                 .dport = __bpf_htons(53)          // port 53
             },
     }};
-
+struct sock_storage_data mock_storage_ipv6 = {
+    .via_waypoint = 1,
+    .has_encoded = 0,
+    .sk_tuple.ipv6 =
+        {
+            // fc00:dead:beef:1234::abcd
+            .daddr =
+                {__bpf_htonl(0xfc00dead), __bpf_htonl(0xbeef1234), __bpf_htonl(0x00000000), __bpf_htonl(0x0000abcd)},
+            .dport = __bpf_htons(53), // port 53
+        },
+};
 static void *mock_bpf_sk_storage_get(void *map, void *sk, void *value, __u64 flags)
 {
     void *storage = NULL;
     storage = bpf_sk_storage_get(map, sk, value, flags);
     if (!storage && map == &map_of_sock_storage) {
-        storage = &mock_storage;
+        struct bpf_sock *sk_sock = (struct bpf_sock *)sk;
+        if (sk_sock->family == AF_INET) {
+            storage = &mock_storage_ipv4;
+        } else {
+            storage = &mock_storage_ipv6;
+        }
     }
     return storage;
 }
