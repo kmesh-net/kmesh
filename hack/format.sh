50c6298832fba445c3da573fd44d1671f63a6345
Tom <yusencao@outlook.com>
2025-08-02 06:50:48 +0000

    suggestion fix
    Signed-off-by: Tom <yusencao@outlook.com>

--- hack/format.sh
+++ hack/format.sh
@@ -36,7 +36,7 @@ if [ -z "$GITHUB_ACTIONS" ]; then
 	docker run --rm \
 		-v "$PWD":/workspace \
 		-w /workspace \
-		--user root \
+		--user "$(id -u):$(id -g)" \
 		davidanson/markdownlint-cli2:v0.18.1 \
 		markdownlint-cli2 --fix "**/*.md"
 fi
