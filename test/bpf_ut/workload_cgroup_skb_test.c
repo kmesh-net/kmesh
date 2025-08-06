7f5a8952b6120da90fb7bb3942bea7b42cd72631
sweeywu <sweetwx6@gmail.com>
2025-08-06 21:36:37 +0800

    add_cgroup_ipv4_ingress_and_egress
    Signed-off-by: sweeywu <sweetwx6@gmail.com>

---
+++ test/bpf_ut/workload_cgroup_skb_test.c
@@ -0,0 +1,30 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include "bpf_log.h"
+#include "bpf_common.h"
+struct sock_storage_data mock_storage = {
+    .connect_ns = 0,
+    .direction = 1,
+    .connect_success = 1,
+    .via_waypoint = 1,
+    .has_encoded = 0,
+    .last_report_ns = 5 * 1000000000ULL,
+    .sk_tuple = {
+        .ipv4 = {
+            .daddr = __bpf_htonl(0x08080808), // 8.8.8.8
+            .dport = __bpf_htons(53),         // 53
+        }}};
+static void *mock_bpf_sk_storage_get(void *map, void *sk, void *value, __u64 flags)
+{
+    struct bpf_sock *sk_sock = (struct bpf_sock *)sk;
+    void *storage = NULL;
+    storage = bpf_sk_storage_get(map, sk_sock, value, flags);
+    if (!storage && map == &map_of_sock_storage) {
+        storage = &mock_storage;
+    }
+
+    return storage;
+}
+
+#define bpf_sk_storage_get mock_bpf_sk_storage_get
+#include "workload/cgroup_skb.c"
