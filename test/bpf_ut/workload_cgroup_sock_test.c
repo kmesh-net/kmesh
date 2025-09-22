63193d4f144a8f78ce9e4a6548e5b55e7f5d1ab8
sweeywu <sweetwx6@gmail.com>
2025-09-22 17:57:53 +0800

    add tail call
    Signed-off-by: sweeywu <sweetwx6@gmail.com>

--- test/bpf_ut/workload_cgroup_sock_test.c
+++ test/bpf_ut/workload_cgroup_sock_test.c
@@ -3,14 +3,13 @@
 #include "bpf_log.h"
 #include "bpf_common.h"
 
-
-SEC("cgroup/connect4") 
+SEC("cgroup/connect4")
 int cgroup_connect4_tail(struct bpf_sock_addr *ctx)
 {
     BPF_LOG(INFO, KMESH, ">> Tail called into cgroup_connect_tail!\n");
     return 1;
 }
-SEC("cgroup/connect6") 
+SEC("cgroup/connect6")
 int cgroup_connect6_tail(struct bpf_sock_addr *ctx)
 {
     BPF_LOG(INFO, KMESH, ">> Tail called into cgroup_connect_tail!\n");
