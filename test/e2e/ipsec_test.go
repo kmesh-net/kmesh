433c9c9b7f2092b13e7cd11ac1573317c1f8f3d5
xiaojiangao123 <2061389208@qq.com>
2025-10-27 01:23:12 +0800

    add secret create
    Signed-off-by: xiaojiangao123 <2061389208@qq.com>

--- test/e2e/ipsec_test.go
+++ test/e2e/ipsec_test.go
@@ -1,3 +1,26 @@
+//go:build integ
+// +build integ
+
+/*
+ * Copyright The Kmesh Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// NOTE: THE CODE IN THIS FILE IS MAINLY REFERENCED FROM ISTIO INTEGRATION
+// FRAMEWORK(https://github.com/istio/istio/tree/master/tests/integration)
+// AND ADAPTED FOR KMESH.
+
 package kmesh
 
 import (
@@ -326,9 +349,15 @@ func rotateIPSecKeyAndWait(t framework.TestContext, containerName string, interv
 	}
 }
 
-func TestIPsecAuthorization(t *testing.T) {
+func TestIPsecConnectivityAndKeyRotation(t *testing.T) {
 	framework.NewTest(t).Run(func(t framework.TestContext) {
 		t.NewSubTest("IPsec Authorization").Run(func(t framework.TestContext) {
+			secretCmd := exec.Command("kmeshctl", "secret", "create")
+            out, err := secretCmd.CombinedOutput()
+            if err != nil {
+                t.Fatalf("Failed to rotate IPSec key: %v\n%s", err, string(out))
+                return
+            }
 
 			// prepare params and deploy
 			if err := labelNamespace(t, "default", "istio.io/dataplane-mode", "Kmesh"); err != nil {
