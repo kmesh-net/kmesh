b2a709cbe346b97a0f839468411e5ec12844ee20
Shubham Pingale <pingaleshubham600@gmail.com>
2026-04-09 13:06:21 +0000

    chore: trigger CI re-run for flaky e2e
    Signed-off-by: Shubham Pingale <pingaleshubham600@gmail.com>

--- pkg/constants/constants_test.go
+++ pkg/constants/constants_test.go
@@ -26,7 +26,7 @@ func TestConstants(t *testing.T) {
 		got      interface{}
 		expected interface{}
 	}{
-		// Models
+		// Modes
 		{"KernelNativeMode", KernelNativeMode, "kernel-native"},
 		{"DualEngineMode", DualEngineMode, "dual-engine"},
 
@@ -41,7 +41,7 @@ func TestConstants(t *testing.T) {
 		{"DISABLED", DISABLED, uint32(0)},
 		{"XfrmDecryptedMark", XfrmDecryptedMark, int(0x00d0)},
 		{"XfrmEncryptMark", XfrmEncryptMark, int(0x00e0)},
-		{"XfrmMarkMask", XfrmMarkMask, int(0xffffffff)},
+		{"XfrmMarkMask", XfrmMarkMask, uint32(0xffffffff)},
 		{"TC_MARK_DECRYPT", TC_MARK_DECRYPT, "tc_mark_decrypt"},
 		{"TC_MARK_ENCRYPT", TC_MARK_ENCRYPT, "tc_mark_encrypt"},
 		{"TC_ATTACH", TC_ATTACH, int(0)},
